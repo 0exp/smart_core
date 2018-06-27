94ec06f294c08031c8ed8ef6b3650d957727cf76
Rustam Ibragimov <iamdaiver@icloud.com>
2018-06-27 22:22:45 +0300

    RSpec+simplecov+coveralls configuration

--- spec/smart_core_spec.rb
+++
@@ -1,9 +0,0 @@
-RSpec.describe SmartCore do
-  it "has a version number" do
-    expect(SmartCore::VERSION).not_to be nil
-  end
-
-  it "does something useful" do
-    expect(false).to eq(true)
-  end
-end
