be38e1b81d7b9a96daf1d2f2625f4f3a58ac8607
Rustam Ibragimov <iamdaiver@gmail.com>
2019-02-27 18:03:22 +0300

    [initializer] inclusion logic

--- lib/smart_core/initializer.rb
+++ lib/smart_core/initializer.rb
@@ -9,4 +9,15 @@ module SmartCore::Initializer
   require_relative 'initializer/attribute_definer'
   require_relative 'initializer/instance_builder'
   require_relative 'initializer/initialization_dsl'
+
+  class << self
+    # @param child_klass [Class]
+    # @return [void]
+    #
+    # @api public
+    # @since 0.5.0
+    def included(child_klass)
+      child_klass.include(InitializationDSL)
+    end
+  end
 end
