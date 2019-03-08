27ec9aa67425936daecf54b831d3271d00f41041
Rustam Ibragimov <iamdaiver@gmail.com>
2019-03-08 20:52:41 +0300

    [container] thread safe dependency caller

--- lib/smart_core/container/dependency.rb
+++ lib/smart_core/container/dependency.rb
@@ -4,12 +4,14 @@
 # @since 0.5.0
 class SmartCore::Container::Dependency < SmartCore::Container::Entity
   # @param dependency_definition [Proc]
+  # @param options [Hash<Symbol,Any>]
   # @return [void]
   #
   # @api private
   # @since 0.5.0
-  def initialize(dependency_definition)
+  def initialize(dependency_definition, **options)
     @dependency_definition = dependency_definition
+    @access_lock = Mutex.new
   end
 
   # @return [Any]
@@ -17,8 +19,8 @@ class SmartCore::Container::Dependency < SmartCore::Container::Entity
   # @api private
   # @since 0.5.0
   def call
-    # TODO: add memoization abilities
-    dependency_definition.call
+    # TODO: support for memoization
+    thread_safe { dependency_definition.call }
   end
 
   private
@@ -28,4 +30,19 @@ class SmartCore::Container::Dependency < SmartCore::Container::Entity
   # @api private
   # @since 0.5.0
   attr_reader :dependency_definition
+
+  # @return [Boolean]
+  #
+  # @api private
+  # @since 0.5.0
+  attr_reader :memoize
+
+  # @param block [Proc]
+  # @return [Any]
+  #
+  # @api private
+  # @since 0.5.0
+  def thread_safe(&block)
+    @access_lock.synchronize(&block)
+  end
 end
