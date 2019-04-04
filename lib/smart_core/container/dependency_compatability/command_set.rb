105d0d1b56dc57eb85fc9363e0ebb14f1c66c3e9
Rustam Ibragimov <iamdaiver@gmail.com>
2019-04-04 23:38:23 +0300

    [container] dependency compatability modules refactoring

--- lib/smart_core/container/dependency_compatability/command_set.rb
+++ lib/smart_core/container/dependency_compatability/command_set.rb
@@ -6,60 +6,28 @@ module SmartCore::Container::DependencyCompatability
   class CommandSet < Abstract
     class << self
       # @param command_set [SmartCore::Container::CommandSet]
-      # @param dependency_command [SmartCore::Container::Commands::Register]
-      # @return [void]
-      #
-      # @raise [SmartCore::Container::NamespaceOverlapError]
-      #
-      # @api private
-      # @since 0.5.0
-      def prevent_namespace_overlap!(command_set, dependency_command)
-        raise(
-          SmartCore::Container::NamespaceOverlapError,
-          "Trying to overlap already registered :#{dependency_command.dependency_name} " \
-          "namespace with :#{dependency_command.dependency_name} dependency!"
-        ) if potential_namespace_overlap?(command_set, dependency_command)
-      end
-
-      # @param command_set [SmartCore::Container::CommandSet]
-      # @param namespace_command [SmartCore::Container::Commands::Namespace]
-      # @return [void]
-      #
-      # @raise [SmartCore::Container::DependencyOverlapError]
-      #
-      # @api private
-      # @since 0.5.0
-      def prevent_dependency_overlap!(command_set, namespace_command)
-        raise(
-          SmartCore::Container::DependencyOverlapError,
-          "Trying to overlap already registered :#{namespace_command.namespace_name} " \
-          "dependency with :#{namespace_command.namespace_name} namespace!"
-        ) if potential_dependency_overlap?(command_set, namespace_command)
-      end
-
-      # @param command_set [SmartCore::Container::CommandSet]
-      # @param dependency_command [SmartCore::Container::Commands::Register]
+      # @param dependency_name [String]
       # @return [Boolean]
       #
       # @api private
       # @since 0.5.0
-      def potential_namespace_overlap?(command_set, dependency_command)
+      def potential_namespace_overlap?(command_set, dependency_name)
         command_set.any? do |command|
-          next unless command.is_a?(SmartCore::Container::Command::Namespace)
-          command.namespace_name == dependency_command.dependency_name
+          next unless command.is_a?(SmartCore::Container::Commands::Namespace)
+          command.namespace_name == dependency_name
         end
       end
 
       # @param command_set [SmartCore::Container::CommandSet]
-      # @param namespace_command [SmartCore::Container::Commands::Namespace]
+      # @param namespace_name [String]
       # @return [Boolean]
       #
       # @api private
       # @since 0.5.0
-      def potential_dependency_overlap?(command_set, namespace_command)
+      def potential_dependency_overlap?(command_set, namespace_name)
         command_set.any? do |command|
-          next unless command.is_a?(SmartCore::Container::Command::Register)
-          command.dependency_name == command.namespace_name
+          next unless command.is_a?(SmartCore::Container::Commands::Register)
+          command.dependency_name == namespace_name
         end
       end
     end
--- lib/smart_core/container/dependency_compatability/registry.rb
+++ lib/smart_core/container/dependency_compatability/registry.rb
@@ -5,63 +5,31 @@ module SmartCore::Container::DependencyCompatability
   # @since 0.5.0
   class Registry < Abstract
     class << self
-      # @param registry [SmartCore::Container::Registry]
-      # @param dependency [SmartCore::Container::Dependency]
-      # @return [void]
-      #
-      # @raise [SmartCore::Container::NamespaceOverlapError]
-      #
-      # @api private
-      # @since 0.5.0
-      def prevent_namespace_overlap!(registry, dependency)
-        raise(
-          SmartCore::Container::NamespaceOverlapError,
-          "Trying to overlap already registered :#{dependency.external_name} " \
-          "namespace with :#{dependency.external_name} dependency!"
-        ) if potential_namespace_overlap?(registry, dependency)
-      end
-
-      # @param registry [SmartCore::Container::Registry]
-      # @param namespace [SmartCore::Container::Namespace]
-      # @return [void]
-      #
-      # @raise [SmartCore::Container::DependencyOverlapError]
-      #
-      # @api private
-      # @since 0.5.0
-      def prevent_dependency_overlap!(registry, namespace)
-        raise(
-          SmartCore::Container::DependencyOverlapError,
-          "Trying to overlap already registered :#{namespace.external_name} " \
-          "dependency with :#{namespace.external_name} namespace!"
-        ) if potential_dependency_overlap?(registry, namespace)
-      end
-
       # @param registry [SmartCore::Container::Regsitry]
-      # @param dependency [SmartCore::Container::Dependency]
+      # @param dependency_name [String]
       # @return [Boolean]
       #
       # @api private
       # @since 0.5.0
-      def potential_namespace_overlap?(registry, dependency)
+      def potential_namespace_overlap?(registry, dependency_name)
         registry.any? do |registered_dependency|
           next unless registered_dependency.is_a?(SmartCore::Container::Namespace)
           # NOTE: registered_dependency is a namespace
-          dependency.external_name == registered_dependency.external_name
+          registered_dependency.external_name == dependency_name
         end
       end
 
       # @param registry [SmartCore::Container::Regsitry]
-      # @param namespace [SmartCore::Container::Namespace]
+      # @param namespace_name [String]
       # @return [Boolean]
       #
       # @api private
       # @since 0.5.0
-      def potential_dependency_overlap?(registry, namespace)
+      def potential_dependency_overlap?(registry, namespace_name)
         registry.any? do |registered_dependency|
           next unless registered_dependency.is_a?(SmartCore::Container::Dependency)
           # NOTE: registered_dependency is a dependency/memoized dependency
-          namespace.external_name == registered_dependency.external_name
+          registered_dependency.external_name == namespace_name
         end
       end
     end
