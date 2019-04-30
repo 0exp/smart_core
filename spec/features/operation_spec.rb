efd64083eeb9f9ac53b6eb37c4bad0de43f54341
Rustam Ibragimov <iamdaiver@gmail.com>
2019-04-30 03:03:15 +0300

    [initializer] method finalizathion behavior spec

--- spec/features/operation_spec.rb
+++ spec/features/operation_spec.rb
@@ -35,20 +35,43 @@ describe SmartCore::Operation do
     class SmokeFinalizer < SmartCore::Operation
       param :kek, finalize: -> (value) { value.to_s }
       option :pek, finalize: proc { 'fek' }
-      params :a, :b, :c
+      params :a, :b
       options :jek, :fek
     end
 
-    service = SmokeFinalizer.new(1, 2, 3, 4, pek: nil, jek: '55', fek: 33)
+    service = SmokeFinalizer.new(1, 2, 3, pek: nil, jek: '55', fek: 33)
 
     expect(service.kek).to eq('1')
     expect(service.pek).to eq('fek')
     expect(service.a).to eq(2)
     expect(service.b).to eq(3)
-    expect(service.c).to eq(4)
 
     expect(service.jek).to eq('55')
     expect(service.fek).to eq(33)
+
+    class SuperSmokeFinalizer < SmokeFinalizer
+      param :cho, finalize: :finalize_cho
+      option :pho, finalize: :finalize_pho, default: -> { 22 }
+
+      def finalize_cho(value)
+        value.to_s
+      end
+
+      def finalize_pho(value)
+        value.to_s
+      end
+    end
+
+    service = SuperSmokeFinalizer.new(5, 6, 7, :mega_blast, pek: 1, jek: 2, fek: 3)
+
+    expect(service.kek).to eq('5')
+    expect(service.a).to eq(6)
+    expect(service.b).to eq(7)
+    expect(service.pek).to eq('fek')
+    expect(service.jek).to eq(2)
+    expect(service.fek).to eq(3)
+    expect(service.cho).to eq('mega_blast')
+    expect(service.pho).to eq('22')
   end
 
   describe 'attribute definition DSL and service instantiation' do
